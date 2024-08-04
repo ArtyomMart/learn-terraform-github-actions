acb081840fac5a88db1db68c0234fd8f7e1b3cfb
Artyom Martirosyan <Artyom.Martirosyan@vertexinc.com>
2024-08-03 18:16:07 -0700

    changed main

--- main.tf
+++ main.tf
@@ -2,55 +2,23 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.44.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.6.0"
+      version = "~> 3.27"
     }
   }
-  required_version = "~> 1.0"
 
-  backend "remote" {
-    organization = "ACG-Terraform-Demos-Artyom"
-
-    workspaces {
-      name = "demo-github-actions"
-    }
-  }
+  required_version = ">= 0.14.9"
 }
-
 
 provider "aws" {
-  region = "us-east-1"
+  profile = "default"
+  region  = "us-east-1"
 }
 
+resource "aws_instance" "app_server" {
+  ami           = "ami-09e67e426f25ce0d7"
+  instance_type = "t2.micro"
 
-
-resource "random_pet" "sg" {}
-
-resource "aws_instance" "web" {
-  ami                    = "ami-09e67e426f25ce0d7"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, Everyone!" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-}
-
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "ExampleAppServerInstance"
   }
-}
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
 }
